315ed14474304128351018f01645ac9400bd2ed5
nyugoh <nyugoh@gmail.com>
2018-11-28 16:27:20 +0300

    Added register and login endpoints

--- src/models/Hotel.js
+++ src/models/Hotel.js
@@ -2,14 +2,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const HotelSchema = new Schema({
-  name: {
+  applicantName: {
     type: String,
     required: true
   },
-  slug: { type: String },
-  blogs: {
+  businessEmail: {
+    type: String,
+    required: true
+  },
+  mobileNumber: {
     type: Number,
-    default: 0
+    required: true
+  },
+  businessName: {
+    type: String,
+    required: true
+  },
+  payBillNo: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  paymentStatus: {
+    type: String,
+    default: 'UNPAID'
   }
 }, { timestamps: true });
 
--- src/routes/hotel.js
+++ src/routes/hotel.js
@@ -1,12 +1,52 @@
 const router = require('express').Router();
 const Hotel = require('../models/Hotel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-
-router.get('/hotels/register', (req, res) => {
-    res.json({ message: 'Welcome post some data to /hotels/register' });
+router.post('/register', (req, res) => {
+    if (req.body === undefined) {
+      throw new Error('A request body is required');
+    }
+    let hotel = new Hotel(req.body);
+    bcrypt.hash(hotel.password, 10).then((hash) => {
+      hotel.password = hash;
+      return hotel.save();
+    }).then((hotel) => {
+      // TODO:: Send verification email ~ via a message broker
+      res.json({ success: true, hotel: hotel });
+    }).catch((e) => {
+      res.status(404).json({ success: false, message: e.message });
+    });
 });
 
+router.post('/login', (req, res) => {
+  try {
+    if (req.body.email === undefined || req.body.password === undefined) {
+      throw new Error('A email & password are required');
+    }
+  } catch (e) {
+    return res.status(404).json({ success: false, message: e.message });
+  }
+  let hotel;
+  Hotel.findOne({ businessEmail: req.body.email }).then((h) => {
+    hotel = h;
+    if (hotel) return bcrypt.compare(req.body.password, hotel.password);
+    else throw new Error('Business email doesn\'t exist');
+  }).then((status) => {
+    if (status) {
+      let token = jwt.sign({
+        email: hotel.businessEmail,
+        id: hotel._id
+      }, process.env.SESSIONKEY);
+      res.json({ success: true, token });
+    } else {
+      throw new Error('Invalid email/password');
+    }
+  }).catch((e) => {
+    res.status(404).json({ success: false, message: e.message });
+  });
+});
 
-module.exports = app => {
+module.exports = (app) => {
     app.use('/', router);
 }
