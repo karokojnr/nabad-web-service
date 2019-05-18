d837c6c546fed9da737796053456d20233e9f44c
kenkarolz99 <kenkarolz99@gmail.com>
2019-05-18 18:08:48 +0300

    WEB SERVICE

--- src/routes/hotel.js
+++ src/routes/hotel.js
@@ -11,17 +11,8 @@ const multer = require('multer');
 const path = require('path');
 const crypto = require('crypto');
 
-const storage = multer.diskStorage({
-  destination: 'public/images/uploads/hotels',
-  filename: function (req, file, cb) {
-    crypto.pseudoRandomBytes(16, function (err, raw) {
-      if (err) return cb(err)
 
-      cb(null, raw.toString('hex') + path.extname(file.originalname))
-    })
-  }
-});
-const upload = multer({ storage: storage });
+
 
 router.get('/hotels', (req, res) => {
   Hotel.find().then((h) => {
